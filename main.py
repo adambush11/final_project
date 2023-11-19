3849f94bd67cc78794329addffc068c5028c40aa
adambush11 <150747181+adambush11@users.noreply.github.com>
2023-11-19 16:32:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-import datetime
-
+from datetime import datetime
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
 from flask import Flask, render_template
+import io
+import base64
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2023, 11, 14)]
-    #dummy_times = [
-        #datetime.datetime(2018, 1, 1, 10, 0, 0),
-        #datetime.datetime(2018, 1, 2, 10, 30, 0),
-        #datetime.datetime(2018, 1, 3, 11, 0, 0),
-    #]
+    # Set time period
+    start = datetime(2018, 1, 1)
+    end = datetime(2018, 12, 31)
 
-    return render_template("index.html", times=dummy_times)
+    # Create Point for Vancouver, BC
+    location = Point(49.2497, -123.1193, 70)
 
+    # Get daily data for 2018
+    data = Daily(location, start, end)
+    data = data.fetch()
+
+    # Plot line chart including average, minimum and maximum temperature
+    plt.plot(data.index, data['tavg'], label='Average Temperature')
+    plt.plot(data.index, data['tmin'], label='Minimum Temperature')
+    plt.plot(data.index, data['tmax'], label='Maximum Temperature')
+    plt.legend()
+    plt.xlabel('Date')
+    plt.ylabel('Temperature (°C)')
+
+    # Save the plot to a BytesIO object
+    img_buffer = io.BytesIO()
+    plt.savefig(img_buffer, format='png')
+    img_buffer.seek(0)
+
+    # Encode the image in base64
+    img_str = base64.b64encode(img_buffer.read()).decode('utf-8')
+
+    return render_template("index.html", img_data=img_str)
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host="127.0.0.1", port=8080, debug=True)
